292e4cb4a9427bbdf163cff4d17212aa8f5c9d80
Yuya Kaido <yuyakaido@gmail.com>
2015-09-07 10:19:35 +0900

    Implement RRuleProcessor and RRuleProcessorTest

--- rrule-processor/src/main/java/com/yuyakaido/android/rruleprocessor/RRuleProcessor.java
+++ rrule-processor/src/main/java/com/yuyakaido/android/rruleprocessor/RRuleProcessor.java
@@ -1,10 +1,50 @@
 package com.yuyakaido.android.rruleprocessor;
 
+import android.text.format.Time;
+
+import com.android.calendarcommon2.DateException;
+import com.android.calendarcommon2.EventRecurrence;
+import com.android.calendarcommon2.RecurrenceProcessor;
+import com.android.calendarcommon2.RecurrenceSet;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by yuyakaido on 9/4/15.
  */
 public final class RRuleProcessor {
 
     private RRuleProcessor() {}
+
+    public static List<Date> getDates(String rule, Date dtStart, Date from, Date to) {
+        Time dtStartTime = date2Time(dtStart);
+        Time fromTime = null;
+        if (dtStart.after(from)) {
+            fromTime = date2Time(dtStart);
+        } else {
+            fromTime = date2Time(from);
+        }
+        Time toTime = date2Time(to);
+        List<Date> result = new ArrayList<>();
+        try {
+            RecurrenceSet recurrenceSet = new RecurrenceSet(rule, null, null, null);
+            RecurrenceProcessor processor = new RecurrenceProcessor();
+            long[] dates = processor.expand(dtStartTime, recurrenceSet, fromTime.toMillis(true), toTime.toMillis(true));
+            for (long l : dates) {
+                result.add(new Date(l));
+            }
+        } catch (DateException | EventRecurrence.InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static Time date2Time(Date date) {
+        Time time = new Time("UTC");
+        time.set(date.getTime());
+        return time;
+    }
 
 }
---
+++ rrule-processor/src/test/java/com/yuyakaido/android/rruleprocessor/RRuleProcessorTest.java
@@ -0,0 +1,353 @@
+package com.yuyakaido.android.rruleprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by yuyakaido on 9/6/15.
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, manifest = Config.NONE)
+public class RRuleProcessorTest {
+
+    @Test
+    public void testAll() {
+        testDaily();
+        testWeekly();
+        testMonthly();
+        testYearly();
+        testUntil();
+        testBeforeByDay();
+        testAfterByDay();
+        testMultipleByDay();
+        testEndOfMonth();
+    }
+
+    private void testDaily() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-20T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-20T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-21T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-22T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-23T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-24T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-25T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-26T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-27T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-28T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-29T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-30T00:00:00.000-0000"));
+            String rule = "FREQ=DAILY";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testWeekly() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-13T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-20T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-27T00:00:00.000-0000"));
+            String rule = "FREQ=WEEKLY";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testMonthly() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-01-01T00:00:00.000-0000");
+            Date to = df.parse("2015-12-31T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-10-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-11-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-12-06T00:00:00.000-0000"));
+            String rule = "FREQ=MONTHLY";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testYearly() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2010-01-01T00:00:00.000-0000");
+            Date to = df.parse("2020-12-31T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2016-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2017-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2018-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2019-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2020-09-06T00:00:00.000-0000"));
+            String rule = "FREQ=YEARLY";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testUntil() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-13T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-20T00:00:00.000-0000"));
+            String rule = "FREQ=WEEKLY;UNTIL=20150925T000000Z";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testBeforeByDay() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-12T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-19T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-26T00:00:00.000-0000"));
+            String rule = "FREQ=WEEKLY;BYDAY=SA";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testMultipleByDay() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-07T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-12T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-13T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-14T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-19T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-20T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-21T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-26T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-27T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-28T00:00:00.000-0000"));
+            String rule = "FREQ=WEEKLY;BYDAY=SA,SU,MO";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testAfterByDay() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-09-01T00:00:00.000-0000");
+            Date to = df.parse("2015-09-30T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-09-06T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-09-06T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-07T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-14T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-21T00:00:00.000-0000"));
+            expect.add(df.parse("2015-09-28T00:00:00.000-0000"));
+            String rule = "FREQ=WEEKLY;BYDAY=MO";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testEndOfMonth() {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date from = df.parse("2015-01-01T00:00:00.000-0000");
+            Date to = df.parse("2015-12-31T23:59:59.999-0000");
+            Date dtStart = df.parse("2015-10-31T00:00:00.000-0000");
+
+            List<Date> expect = new ArrayList<>();
+            expect.add(df.parse("2015-10-31T00:00:00.000-0000"));
+            expect.add(df.parse("2015-12-31T00:00:00.000-0000"));
+            String rule = "FREQ=MONTHLY";
+            List<Date> actual = RRuleProcessor.getDates(rule, dtStart, from, to);
+
+            String message = "RRule = " + rule + ", dtStart = " + df.format(dtStart);
+            println(message);
+            message = "expect.size() = " + expect.size() + ", actual.size() = " + actual.size();
+            println(message);
+            assertTrue(message, expect.size() == actual.size());
+
+            for (int i = 0; i < expect.size(); i++) {
+                message = "expect = " + df.format(expect.get(i)) + ", actual = " + df.format(actual.get(i));
+                println(message);
+                assertTrue(expect.get(i).equals(actual.get(i)));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+
+}
